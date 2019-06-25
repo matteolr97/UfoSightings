88f55a9dde404b515e7d21397beee4973116557a
matte <matte@DESKTOP-JA42G9P>
2019-06-25 21:48:32 +0200

    punto 1 completo

--- src/it/polito/tdp/ufo/db/DBConnect.java
+++ src/it/polito/tdp/ufo/db/DBConnect.java
@@ -17,6 +17,7 @@ public class DBConnect {
 			HikariConfig config = new HikariConfig();
 			config.setJdbcUrl(jdbcURL);
 			config.setUsername("root");
+			
 			config.setPassword("admin");
 			
 			// configurazione MySQL
